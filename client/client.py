6a3128bb28e98834e3f0843a1b97e59c78f77530
Eric <ecarstensen99@gmail.com>
2022-11-22 12:13:36 -0700

    Moved keys

--- client/client.py
+++ client/client.py
@@ -27,15 +27,9 @@ def client():
         connectionSocket.connect((serverName,serverPort))
 
         # Client receives a message and send it to the client
-        message = connectionSocket.recv(2048).decode('ascii')
-
-        #Client send message to the server
-        message = input(message).encode('ascii')
-        connectionSocket.send(message)
-
-        # Client receives a message from the server and print it
         message = connectionSocket.recv(2048)
-        print(message.decode('ascii'))
+
+        message = priv_decrypt(message, )
 
         # Client terminate connection with the server
         connectionSocket.close()
@@ -44,6 +38,41 @@ def client():
         print('An error occured:',e)
         connectionSocket.close()
         sys.exit(1)
+
+
+
+
+
+
+#Takes a string and returns a symetric encrypted binary
+def sym_encrypt(message, key):
+    #Generate cipher block
+    cipher = AES.new(key, AES.MODE_ECB)
+    # Encrypt the message
+    ct_bytes = cipher.encrypt(pad(message.encode('ascii'),16))
+    return ct_bytes
+
+#Takes an encrypted binary and returns a Decrypted string
+def sym_decrypt(message, key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    Padded_message = cipher.decrypt(message)
+    #Remove padding
+    Encodedmessage = unpad(Padded_message,16)
+    return (Encodedmessage.decode('ascii'))
+
+#Takes a string and a public key returns a public encrypted binary
+def pub_encrypt(message, key):
+    cipher_rsa_en = PKCS1_OAEP.new(key)
+    enc_data = cipher_rsa_en.encrypt(message.encode('ascii'))
+    return(enc_data)
+
+#Takes a public encrypted binary and a private key and returns a Decrypted string
+def priv_decrypt(message, key):
+    cipher_rsa_dec = PKCS1_OAEP.new(key)
+    dec_data = cipher_rsa_dec.decrypt(message)
+    print(dec_data.decode('ascii'))
+
+
 
 #----------
 client()
