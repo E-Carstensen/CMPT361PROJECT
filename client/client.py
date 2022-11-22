e8b302390b5660bdb802417f9b34014e781edf47
samuel <stbrownlee@shaw.ca>
2022-11-22 18:57:32 +0000

    made comment test

--- client/client.py
+++ client/client.py
@@ -9,37 +9,37 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 
-
+#test comment
 def client():
     # Server Information
     serverName = '127.0.0.1' #'localhost'
     serverPort = 12000
-    
-    #Create client socket that useing IPv4 and TCP protocols 
+
+    #Create client socket that useing IPv4 and TCP protocols
     try:
         connectionSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error as e:
         print('Error in client socket creation:',e)
-        sys.exit(1)    
-    
+        sys.exit(1)
+
     try:
         #Client connect with the server
         connectionSocket.connect((serverName,serverPort))
-        
+
         # Client receives a message and send it to the client
         message = connectionSocket.recv(2048).decode('ascii')
-        
+
         #Client send message to the server
         message = input(message).encode('ascii')
         connectionSocket.send(message)
-        
+
         # Client receives a message from the server and print it
         message = connectionSocket.recv(2048)
         print(message.decode('ascii'))
-        
+
         # Client terminate connection with the server
         connectionSocket.close()
-        
+
     except socket.error as e:
         print('An error occured:',e)
         connectionSocket.close()
